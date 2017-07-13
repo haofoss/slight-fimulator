cec1e6983cd2f1865ea6baf48a0b0e265f60f7e6
MasterofNumbers17 <masterofnumbers17@gmail.com>
2017-07-12 21:22:52 -0400

    Update stable.py

--- stable.py
+++ stable.py
@@ -16,30 +16,6 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
-
-import random
-import os
-import math
-
-import pygame
-
-
-"""Slight Fimulator - Flight simlator in Python
-Copyright (C) 2017 Hao Tian
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 BUG REPORTS
@@ -66,6 +42,12 @@ Dlight simulator
 SLOGAN
 Just a bit of fimulating
 """
+
+import random
+import os
+import math
+
+import pygame
 
 
 __author__ = "Hao Tian"
