ea451ea1e44cb30276f5491576596aeb03f86d1e
Adrien Hopkins <masterofnumbers17@gmail.com>
2018-01-25 19:36:44 -0500

    Modified copyright notice to better comply with GPL standards

--- game.py
+++ game.py
@@ -3,15 +3,18 @@
 """The game code.
 
 Slight Fimulator - Flight simulator in Python
-Copyright (C) 2017 Hao Tian and Adrien Hopkins
+Copyright (C) 2017, 2018 Hao Tian and Adrien Hopkins
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
+
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
@@ -189,7 +192,6 @@ Your score was {}.",
         self.load_resources()
 
         self.screen = pygame.display.set_mode(self.size, pygame.RESIZABLE)
-##        self.screen = pygame.display.set_mode(self.size)
         pygame.display.set_caption("Slight Fimulator v{}".format(__version__))
 
         pygame.mixer.pre_init(44100, -16, 2, 2048)
